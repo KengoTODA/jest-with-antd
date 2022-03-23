939021feef866cc51f9f4841165e69f6ebcf9fb9
Kengo TODA <skypencil@gmail.com>
2022-03-23 18:42:39 +0800

    introduce ant-design/charts to reproduce the test failure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Line } from '@ant-design/charts';
 
-function App() {
+const App: React.FC = () => {
+  const data = [
+    { year: '1991', value: 3 },
+    { year: '1992', value: 4 },
+    { year: '1993', value: 3.5 },
+    { year: '1994', value: 5 },
+    { year: '1995', value: 4.9 },
+    { year: '1996', value: 6 },
+    { year: '1997', value: 7 },
+    { year: '1998', value: 9 },
+    { year: '1999', value: 13 },
+  ];
+
+  const config = {
+    data,
+    width: 800,
+    height: 400,
+    autoFit: false,
+    xField: 'year',
+    yField: 'value',
+    point: {
+      size: 5,
+      shape: 'diamond',
+    },
+    label: {
+      style: {
+        fill: '#aaa',
+      },
+    },
+  };
+
+  let chart;
+
+  // Export Image
+  const downloadImage = () => {
+    chart?.downloadImage();
+  };
+
+  // Get chart base64 string
+  const toDataURL = () => {
+    console.log(chart?.toDataURL());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button type="button" onClick={downloadImage} style={{ marginRight: 24 }}>
+        Export Image
+      </button>
+      <button type="button" onClick={toDataURL}>
+        Get base64
+      </button>
+      <Line {...config} onReady={(chartInstance) => (chart = chartInstance)} />
     </div>
   );
-}
-
+};
 export default App;
